af5a5d2c1c9587a5f9691e7e81fd090258f5f325
Alex Hermann <hermann.alexandre@ahwebdev.fr>
2012-09-17 04:44:41 +0200

    update version

--- AWD_facebook_seo_comments.php
+++ AWD_facebook_seo_comments.php
@@ -3,7 +3,7 @@
 Plugin Name: Facebook AWD Seo Comments
 Plugin URI: http://www.ahwebdev.fr
 Description: This plugin merge Facebook Comments with native Wordpress Comments System and more... Need Facebook AWD All in One plugin.
-Version: 1.5.1
+Version: 1.5.2
 Author: AHWEBDEV
 Author URI: http://www.ahwebdev.fr
 License: AHWEBDEV
